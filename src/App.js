08826243e1d880cfa0fba4f30d7eb0035ebe4614
Jukka-Pekka Keisala <jp@jpkeisala.com>
2019-09-05 13:52:29 +0200

    init

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         sssss ssasasadsss<br></br>sdsds<br></br>
+         Hi There,<br/> Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
